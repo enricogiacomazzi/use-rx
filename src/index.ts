8600f9fc5b9d9a0d1b740fe4ddd860d9cdc0655d
Jack <jack@Jacks-MacBook-Pro.local>
2020-04-21 10:48:52 +0200

    added subscription refresh on input observable change

--- src/index.ts
+++ src/index.ts
@@ -5,15 +5,9 @@ import {Observable} from 'rxjs';
 export function useRx<T>(state$: Observable<T>, initialState: T = null): T {
     const [state, setState] = useState<T>(initialState);
     useEffect(() => {
-        const sub = state$.subscribe(x => {
-            console.log('state', x);
-            setState(x);
-        });
-
-        return () => {
-            sub && sub.unsubscribe();
-        }
-    }, []);
+        const sub = state$.subscribe(setState);
+        return () => sub && sub.unsubscribe();
+    }, [state$]);
 
     return state;
 }
