550caea5b30a0820325342adb0495559d1c7ca79
Jack <jack@Jacks-MacBook-Pro.local>
2020-04-23 12:33:29 +0200

    changed behavior on observable error

--- index.js
+++ index.js
@@ -4,14 +4,9 @@ const react_1 = require("react");
 function useRx(state$, initialState = null) {
     const [state, setState] = react_1.useState(initialState);
     react_1.useEffect(() => {
-        const sub = state$.subscribe(x => {
-            console.log('state', x);
-            setState(x);
-        });
-        return () => {
-            sub && sub.unsubscribe();
-        };
-    }, []);
+        const sub = state$.subscribe(setState);
+        return () => sub && sub.unsubscribe();
+    }, [state$]);
     return state;
 }
 exports.useRx = useRx;
--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import {Observable} from 'rxjs';
 export function useRx<T>(state$: Observable<T>, initialState: T = null): T {
     const [state, setState] = useState<T>(initialState);
     useEffect(() => {
-        const sub = state$.subscribe(setState);
+        const sub = state$.subscribe(setState, e => {throw new Error(e)}, () => sub && sub.unsubscribe());
         return () => sub && sub.unsubscribe();
     }, [state$]);
 
